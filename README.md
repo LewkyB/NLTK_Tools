6a60e3f74d5e87855cad8af5b4473dab83aa60d3
luke92brown <luke92brown@gmail.com>
2020-10-27 18:58:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# NLTK_Tools
+
+Got my hands on a lot of plain text books for training ML from bookcorpus and didn't know what all to do with them.
+
+I hadn't experimented much with grep and quickly wore that out on the files. Then, I eventually moved on to NLTK for something more to do.
+
+## Uses
+
+* Tokenize plain text books into words, cleans the words, then you can create frequency distribution plots or word clouds 
+
+## Downloads
+
+Downloads are all very fast. 
+
+* [download for 2gb zip that unpacks to 6gb, about 19000 books](https://the-eye.eu/public/AI/pile_preliminary_components/books1.tar.gz)
+* [download the 37gb monster with 197000 books](https://the-eye.eu/public/AI/pile_preliminary_components/books3.tar.gz)
+* [links are courtesy of this comment on Github](https://github.com/soskek/bookcorpus/issues/27#issuecomment-716104208)
+
+## Packages
+
+* [NLTK](https://www.nltk.org/)
+* [word_cloud](https://github.com/amueller/word_cloud)
+
+
+## Other resources
+
+https://github.com/NirantK/nlp-python-deep-learning
+
+https://github.com/keon/awesome-nlp#user-content-python
---
+++ main.py
@@ -0,0 +1,79 @@
+import nltk, string
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+def tokenizer(path):
+    raw = open(path, 'rU').read()
+    raw_tokenized_words = nltk.word_tokenize(raw)
+    return raw_tokenized_words
+
+def tag_and_chunk(raw_tokenized_words):
+
+    tagged = nltk.pos_tag(raw_tokenized_words)
+
+    chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP><NN>?}
+                                }<VB.?|IN|DT>+{"""
+
+    chunkParser = nltk.RegexpParser(chunkGram)
+    chunked = chunkParser.parse(tagged)
+
+    return tagged, chunked
+
+def clean_words(words, word_length):
+
+    # collapse case distinction and ignore puncuation
+    words = [w.lower() for w in words if w.isalpha()]
+    # remove stop words
+    words = [w for w in words if w not in stopwords.words('english')]
+    # only consider words greater than
+    words = [w for w in words if len(w) >= word_length]
+
+    return words
+
+def create_FreqDist(words):
+    fdist = nltk.FreqDist(words)
+    return fdist
+
+def print_most_common_FreqDist(fdist, sample_size):
+    for words, frequency in fdist.most_common(20):
+        print(u'{} ; {}'.format(words, frequency))
+
+def plot_FreqDist(fdist, sample_size, is_cumulative):
+    fdist.plot(sample_size, cumulative = is_cumulative)
+
+def show_wordcloud(words,                    # list produced by nltk tokenizer
+                   cloud_width=1000, 
+                   cloud_height=500, 
+                   fig_x=15, 
+                   fig_y=8,
+                   interpolation_type= "bilinear",
+                   axis_on_off= "off"):
+    
+    # list has to be joined for WordCloud
+    text = " ".join(words)
+    
+    wcloud = WordCloud(width = cloud_width, height = cloud_height).generate(text)
+
+    plt.figure(figsize=(fig_x, fig_y))
+    plt.imshow(wcloud, interpolation = interpolation_type)
+    plt.axis(axis_on_off)
+    plt.show()
+
+def main():
+
+    path = "epubtxt/omega-exile.epub.txt"
+    raw_words = tokenizer(path)
+    words = clean_words(raw_words, 3)
+
+    dist = create_FreqDist(words)
+
+    print_most_common_FreqDist(dist, 20)
+    plot_FreqDist(dist, 20, False)
+    show_wordcloud(words)
+
+if __name__ == "__main__":
+    main()
